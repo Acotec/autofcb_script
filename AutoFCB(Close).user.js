eb38b4dc952c3ec0af7b18344c873a8905a7f02f
HARFHO <Harfho77@gmail.com>
2021-07-03 15:31:44 +0000

    Update AutoFCB(Close).user.js

--- AutoFCB(Close).user.js
+++ AutoFCB(Close).user.js
@@ -2,7 +2,6 @@
     var c = getCookie("visited");
     if (c === "yes") {
          window.close();window.close();window.close();window.close();window.close();window.close()
-    } else {
-        setCookie("visited", "yes",10)
     }
+    setCookie("visited", "yes",300)
 })();
