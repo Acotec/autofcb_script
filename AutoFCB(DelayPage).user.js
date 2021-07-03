6345dafbef264e92987ace5a47c12c6a7c8ea484
HARFHO <Harfho77@gmail.com>
2021-07-03 15:37:20 +0000

    Update AutoFCB(DelayPage).user.js

--- AutoFCB(DelayPage).user.js
+++ AutoFCB(DelayPage).user.js
@@ -5,6 +5,7 @@
     var default_host =['express-cut.ovh', 'bitlinks.pw', 'neonlink.net', 'faucet.100count.net', 'bitcoinly.in',
                        'kiiw.icu', 'adbull.me', 'owllink.net', 'pingit.im', 'cashurl.in',
                        'adshort.co', 'aii.sh', 'riful.com','fc.lc','coinfly.io']
+    var newDelay =[]
     var delayOn = GM_SuperValue.get('delayOn', default_host)
     var host = window.location.host.toLowerCase().replace(/https:\/\/|www\.|\[^.*]|\/.*/ig, '')
     var autofcb = /auto(faucet|claim|bitco)/ig.test(host)
@@ -20,8 +21,8 @@
         alreadyVisit = element.innerText.toLowerCase().includes('already visited this link!')
         referrer = document.referrer.replace(/https:\/\/|www\.|\[^.*]|\/.*/ig, '')
         if (error1052 && !(/auto(faucet|claim|bitco)/ig.test(referrer) || delayOn.includes(referrer) || referrer == '')) {
-            delayOn.push(referrer)
-            GM_SuperValue.set('delayOn', delayOn);
+            newDelay.push(referrer)
+            GM_SuperValue.set(' newDelay', newDelay);
         } else if (referrer == '' && autofcb && !(alreadyVisit || delayOn.includes(referrer))){
             window.history.go(-1)
             //window.history.back()
@@ -34,13 +35,15 @@
             addDelayorClose(element)
         }, );
     } else if (back && /auto(faucet|claim|bitco)/ig.test(document.referrer) && !(delayOn.includes(host) || /auto(faucet|claim|bitco)/ig.test(host)) ) {
-        delayOn.push(host)
-        GM_SuperValue.set('delayOn', delayOn);
+        newDelay.push(host)
+        GM_SuperValue.set(' newDelay', newDelay);
     } else if (delayOn.includes(host) && !/adbull/.test(host)) {
         sleep(17);
     } else if(/adbull/.test(host)){
         waitForKeyElements(".btn-primary", (e) => {
-            sleep(17)
-        },true,0,-1 );}
+            if (e){window.location.reload()
+            e.click()}else{sleep(15);}
+        },true,0,-1 );
+    }
 
 })();
