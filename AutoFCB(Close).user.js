b74fbed50c8456bd0ac7ff70eacb1e9806505f4f
HARFHO <Harfho77@gmail.com>
2021-07-03 05:59:58 +0300

    Update AutoFCB(Close).user.js

--- AutoFCB(Close).user.js
+++ AutoFCB(Close).user.js
@@ -1,13 +1,9 @@
 (function () {
     'use strict';
     var navigate = performance.getEntriesByType("navigation")[0].type == 'navigate'
+    var reload = performance.getEntriesByType("navigation")[0].type =='reload'
     var ref_self = /auto(faucet|claim|bitco)/ig.test(document.referrer)
-    var referrer = document.referrer.includes("shortlinks/visited/");
-    if ((navigate && !ref_self) || referrer ) {
-        window.close()
-    }else {
-        waitForKeyElements("div.alert-success", function(e) {
-            window.close();
-        }, true, 10, -1);
+    var visited = document.referrer.includes("shortlinks/visited/");
+    if ((navigate && !ref_self) || (reload && visited) ) {
+        window.close();window.close();window.close();window.close();window.close();window.close()
     }
-})();
