74c6cbcc4713eb1f51fd6736691fcb49a8c7e96f
HARFHO <Harfho77@gmail.com>
2021-07-08 06:44:44 +0200

    Update AutoFCB(Close).user.js

--- AutoFCB(Close).user.js
+++ AutoFCB(Close).user.js
@@ -1,7 +1,19 @@
 (function () {
-    var c = getCookie("visited");
-    if (c === "yes") {
-         window.close();window.close();window.close();window.close();window.close();window.close()
+    function OnPhone(){
+        var c = getCookie("visited");
+        if (c === "yes") {
+            window.close();window.close();window.close();window.close();window.close();window.close()
+        }
+        setCookie("visited", "yes",300)}
+
+    function OnLaptop(){
+        var navigate = performance.getEntriesByType("navigation")[0].type == 'navigate'
+        var reload = performance.getEntriesByType("navigation")[0].type =='reload'
+        var ref_self = /auto(faucet|claim|bitco)/ig.test(document.referrer)
+        var visited = document.referrer.includes("shortlinks/visited/");
+        if ((navigate && !ref_self) || (reload && visited) ) {
+            window.close();window.close();window.close();window.close();window.close();window.close()
+        }
     }
-    setCookie("visited", "yes",300)
+    OnPhone()
 })();
