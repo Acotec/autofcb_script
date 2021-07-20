2a440d5daf7b3120dfafee62b3086881f8183f7e
HARFHO <Harfho77@gmail.com>
2021-07-20 09:28:44 +0600

    Update AutoFCB(NextClicker).user.js

--- AutoFCB(NextClicker).user.js
+++ AutoFCB(NextClicker).user.js
@@ -1,44 +1,16 @@
 (function() {
-    'use strict';
-    // Your code here...
     var href = window.location.href
-    if(href.includes('dogemate.com')||href.includes('faupto.com')){$("#bdt").click()}
-    else if (href.includes('theblogcash')){document.getElementsByClassName("btn btn-info text-white btn-lg next-button")[0].click()}
-    else if (href.includes('orangeptc')){setTimeout( ()=>{document.querySelector("input").click()},4002)}
-    else if (href.includes('prolinks.xyz')||
-             href.includes('linkdesh.xyz')||
-             href.includes('bitsfree.xyz')||
-             href.includes('dgbauto')
-            ){setInterval(()=>{document.querySelector("#mdt").click()},1000)}
-
-    else if(href.includes('step')){document.querySelector("#main-button").disabled=false
-                                   setTimeout(()=>{document.querySelector("#main-button").click()},100)}
-
-    else if(href.includes('shortzzy.in')){setInterval( () => {document.querySelector("#btn6").click()},1000)}
-
-    // else if(href.includes('100count.net')){//window.onload=()=>{
-    //     setInterval(()=>{ try{location.href=document.getElementById('cl1').querySelector('a').href}
-    //                      catch(err){document.getElementsByTagName('button')[0].click()}},1000) } //}
-    else if(href.includes('yoshare.net')){setTimeout(()=>{ try{document.querySelector("#yuidea > input.yu-btn.yu-blue").click()}
-                                                          catch(err){document.querySelector("#btn6").click()}},1000)}
-
-    else if(href.includes('zegtrends.com')){ try{javascript:document.getElementById('cln').submit()}catch(err){
-        btn()}}
-
-    else if(href.includes('techmody.io')||href.includes('ez4mods.com')){
-        setInterval(()=>{yuidea1()},2000)
-    }
-    else if(href.includes('ez4short.com/')){
-        window.onload=(e)=>{e.preventDefault;window.stop()
-                           }}
-    else if(href.includes('forex-gold.net')){
-        waitForKeyElements('a[href*="health"',(e)=>{ window.location=e.getAttribute('href')},true,0)
-        //window.location=document.getElementsByClassName('submitBtn btn btn-primary ')[0].getAttribute('href')
-    }
-    else if(href.includes('healthy4pepole.com')){
-        scrollToForm()
-        waitForKeyElements('.s-btn-f',(e)=>{ window.location=e.getAttribute('href')},true,0)
-    }
-
-
+    if(/dogemate.com|faupto.com/gi.test(href)){$("#bdt").click()}
+    else if (/theblogcash/gi.test(href)){document.getElementsByClassName("btn btn-info text-white btn-lg next-button")[0].click()}
+    else if (/orangeptc/gi.test(href)){setTimeout( ()=>{document.querySelector("input").click()},4002)}
+    else if (/prolinks.xyz|linkdesh.xyz|bitsfree.xyz|dgbauto/gi.test(href)){setInterval(()=>{document.querySelector("#mdt").click()},1000)}
+    else if(/step/gi.test(href)){document.querySelector("#main-button").disabled=false;setTimeout(()=>{document.querySelector("#main-button").click()},100)}
+    else if(/shortzzy.in/gi.test(href)){setInterval( () => {document.querySelector("#btn6").click()},1000)}
+    else if(/yoshare.net/gi.test(href)){setTimeout(()=>{ try{document.querySelector("#yuidea > input.yu-btn.yu-blue").click()}catch(err){document.querySelector("#btn6").click()}},1000)}
+    else if(/zegtrends/gi.test(href)){ try{javascript:document.getElementById('cln').submit()}catch(err){btn()}}
+    else if(/techmodygi.io|ez4mods.com/gi.test(href)){ setInterval(()=>{yuidea1()},2000)}
+    else if(/ez4short.com/gi.test(href)){window.onload=(e)=>{e.preventDefault;window.stop() }}
+    else if(/forex-gold.net|4orex4.com/gi.test(href)){waitForKeyElements('a[href*="health"',(e)=>{ window.location=e.getAttribute('href')},true,0)}
+    else if(/healthy4pepole.com/gi.test(href)){scrollToForm();waitForKeyElements('.s-btn-f',(e)=>{ window.location=e.getAttribute('href')},true,0)
+                                            }
 })();
