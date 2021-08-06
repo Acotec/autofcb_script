de9ef5819a9e09d923e475be91b39abf74bbc91a
HARFHO <Harfho77@gmail.com>
2021-08-06 18:34:53 +0100

    Update Bypass.user.js

--- Bypass.user.js
+++ Bypass.user.js
@@ -1,14 +1,19 @@
 (function() {
     var key=atob(JSON.parse(GM_getResourceText('key')))
-    if(/===$/.test(window.location.href)){
+    if(/\/===$/.test(window.location.href)){
         const baseUrl = 'https://api.yuumari.com/alpha-bypass/';
-        const link = window.location.href.replace('===','');
+        const link = window.location.href.replace(/\/===/,'');
         const u = key;
         const l = link
         fetch(baseUrl, {
             method: 'POST',
             body: new URLSearchParams({u,l})
         }).then(r => r.json())
-            .then((d)=>{location=d.result});
-    }
+            .then((d)=>{
+            if(!d.message){
+                location=d.result
+            }else{
+                alert(d.message)
+            }
+        });}
 })();
