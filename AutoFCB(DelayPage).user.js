9441296e3c2cce530f7127048c362b1556c65a1a
HARFHO <Harfho77@gmail.com>
2021-08-17 00:41:31 +0100

    Update AutoFCB(DelayPage).user.js

--- AutoFCB(DelayPage).user.js
+++ AutoFCB(DelayPage).user.js
@@ -4,11 +4,12 @@
     var element, error1052, alreadyVisit, success, referrer;
     var default_host =['express-cut.ovh', 'bitlinks.pw', 'neonlink.net', 'faucet.100count.net', 'bitcoinly.in',
                        'kiiw.icu', 'adbull.me', 'owllink.net', 'pingit.im', 'cashurl.in',
-                       'adshort.co', 'aii.sh', 'riful.com','fc.lc','coinfly.io']
+                       'adshort.co', 'aii.sh', 'riful.com','fc-lc','coinfly.io']
     var newDelay =[]
     var delayOn = GM_SuperValue.get('delayOn', default_host)
     var host = window.location.host.toLowerCase().replace(/https:\/\/|www\.|\[^.*]|\/.*/ig, '')
-    var autofcb = /auto(faucet|claim|bitco)/ig.test(host)
+    var autofcb = /auto(faucet|claim|bitco).(in|org).*/ig
+    var isHost = autofcb.test(host)
     var back = String(window.performance.getEntriesByType("navigation")[0].type) === "back_forward"
 
     function sleep(e) {
@@ -20,10 +21,10 @@
         error1052 = element.innerText.toLowerCase().includes('action is marked as suspicious')
         alreadyVisit = element.innerText.toLowerCase().includes('already visited this link!')
         referrer = document.referrer.replace(/https:\/\/|www\.|\[^.*]|\/.*/ig, '')
-        if (error1052 && !(/auto(faucet|claim|bitco)/ig.test(referrer) || delayOn.includes(referrer) || referrer == '')) {
+        if (error1052 && !(autofcb.test(referrer) || delayOn.includes(referrer) || referrer == '')) {
             newDelay.push(referrer)
             GM_SuperValue.set(' newDelay', newDelay);
-        } else if (referrer == '' && autofcb && !(alreadyVisit || delayOn.includes(referrer))){
+        } else if (referrer == '' && isHost && !(alreadyVisit || delayOn.includes(referrer))){
             window.history.go(-1)
             //window.history.back()
         } else {
@@ -35,15 +36,14 @@
         null!=sessionStorage.getItem("reloaded")||sleep(17);
         sessionStorage.setItem('reloaded', 'yes');
     }
-    if (autofcb) {
+    if (isHost) {
         waitForKeyElements("div.alert-danger", (element) => {
             addDelayorClose(element)
         }, );
-    } else if (back && /auto(faucet|claim|bitco)/ig.test(document.referrer) && !(delayOn.includes(host) || /auto(faucet|claim|bitco)/ig.test(host)) ) {
+    } else if (back && autofcb.test(document.referrer) && !(delayOn.includes(host) || autofcb.test(host)) ) {
         newDelay.push(host)
         GM_SuperValue.set(' newDelay', newDelay);
-    } else if (delayOn.includes(host) && !/adbull/.test(host)) {
+    } else if (RegExp(host,'ig').test(delayOn) && !/adbull/.test(host)) {
         sleep(17);
     }
-
 })();
