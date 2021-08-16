a2c5b967644c3dadcb40777bc57a143cf63bcbd7
HARFHO <Harfho77@gmail.com>
2021-08-16 09:30:30 +0100

    Update AutoFCB(2).user.js

--- AutoFCB(2).user.js
+++ AutoFCB(2).user.js
@@ -1,12 +1,6 @@
 (function () {
     'use strict';
-    var getDontOpenList = [];
-    for (let e of GM_getResourceText("_DontOpen").split(",")) getDontOpenList.push(e);
-    const _DontOpen = [];
-    for (let e of getDontOpenList) {
-        let n = e.replace(/\W/gi, "");
-        _DontOpen.includes(n) || _DontOpen.push(n)
-    }
+    const _DontOpen =JSON.parse(GM_getResourceText("_DontOpen").replace(/'/ig,'"'));
     var _open_link_fast = []
     var _alreadyRun = GM_getValue("_alreadyRun")
     var _available_link = parseInt(document.getElementsByClassName('amount')[1].textContent)
@@ -268,6 +262,7 @@
     button.addEventListener("click", function () {
         checkButton()
     });
+    //////////////////
     pageR()
     button.innerHTML = "Script Not Running -- SHORTLINKS=" + _views_ToVisit.length;
     reloadP()
