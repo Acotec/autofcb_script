2eed25075ff7b9c5fd18c15ffa70bff931f8c130
HARFHO <Harfho77@gmail.com>
2021-08-25 02:07:49 +0100

    Update AutoFCB(ReloadShortlinkPage).user.js

--- AutoFCB(ReloadShortlinkPage).user.js
+++ AutoFCB(ReloadShortlinkPage).user.js
@@ -1,22 +1,27 @@
 (function() {
     'use strict';
     function refresh(){
-        //let tryagain = GM_getValue('tryagain',1)
         let tryagain;
         tryagain=sessionStorage.getItem('tryagain')
         if(sessionStorage.getItem('tryagain')==null){sessionStorage.setItem('tryagain',1);tryagain=sessionStorage.getItem('tryagain')}
-        if(parseInt(tryagain)<=3){
+        if(parseInt(tryagain)<=4){
             sessionStorage.setItem('tryagain',parseInt(tryagain)+1);
-            GM_addElement(document.getElementsByTagName('head')[0], 'meta', {'http-equiv':"refresh",'content':"180;url="+window.location.href,});}
+            GM_addElement(document.getElementsByTagName('head')[0], 'meta', {'http-equiv':"refresh",'content':"180000;url="+window.location.href,});}
         else{
             sessionStorage.removeItem('tryagain')
             //console.log('reset')
-            GM_notification({text:window.location.href+' Reloaded 3 times and close',title:window.location.host,timeout:0},null);
-            window.close()
+            GM_notification({text:window.location.href+' Reloaded 4 times and close',
+                             title:window.location.host,
+                             timeout:300*1000,
+                             ondone:()=>{window.close()},
+                             onclick:()=>{window.open(window.location.href);window.close()}
+                            });
+            setTimeout(()=>{window.close()},300*1000)
         }
     }
     var autofcb = /auto(faucet|claim|bitco).(in|org)/ig.test(window.location.href)
     if(!autofcb){
+        //sessionStorage.setItem('page',window.location.url)
         refresh()
     }
 })();
