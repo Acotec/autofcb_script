b8fa75251971b491478fc607a8201a109509eeef
HARFHO <Harfho77@gmail.com>
2021-08-27 20:42:00 +0100

    Update AutoFCB(ReloadShortlinkPage).user.js

--- AutoFCB(ReloadShortlinkPage).user.js
+++ AutoFCB(ReloadShortlinkPage).user.js
@@ -3,25 +3,28 @@
     function refresh(){
         let tryagain;
         tryagain=sessionStorage.getItem('tryagain')
-        if(sessionStorage.getItem('tryagain')==null){sessionStorage.setItem('tryagain',1);tryagain=sessionStorage.getItem('tryagain')}
+        if(sessionStorage.getItem('tryagain')==null){sessionStorage.setItem('tryagain',1);tryagain=sessionStorage.getItem('tryagain')};
         if(parseInt(tryagain)<=4){
-            sessionStorage.setItem('tryagain',parseInt(tryagain)+1);
-            GM_addElement(document.getElementsByTagName('head')[0], 'meta', {'http-equiv':"refresh",'content':"180;url="+window.location.href,});} //content-in-seconds
+            GM_addElement(document.getElementsByTagName('head')[0], 'meta',{'http-equiv':"refresh",'content':"180;url="+window.location.href,});//content-in-seconds
+            sessionStorage.setItem('tryagain',parseInt(tryagain)+1)
+        }
         else{
             sessionStorage.removeItem('tryagain')
+            delete(sessionStorage.tryagain)
             //console.log('reset')
             GM_notification({
                 title:window.location.host,
-                text:window.location.href+' \n--Reloaded 4time and close---',
-                timeout:300*1000,
-                ondone:()=>{window.close()},
-                onclick:()=>{window.open(window.location.href);window.close()}
+                text:window.location.href +' \nReloaded 4-times and will close after 10 seconds ---',
+                timeout:10*1000,
+                ondone:()=>{delete(sessionStorage.tryagain);window.close()},
+                onclick:()=>{delete(sessionStorage.tryagain);window.open(window.location.href);window.close()}
             });
-            GM_setClipboard(window.location.href,{type:'text/plain'})//data,info
-            window.close()
+            delete(sessionStorage.tryagain);
+            GM_setClipboard(window.location.href,'text/plain')//data,info
+            setTimeout(()=>{window.close()},10*1000)
         }
     }
-    var autofcb = /auto(faucet|claim|bitco).(in|org).+|.+\/===/ig.test(window.location.href)
+    var autofcb = /auto(faucet|claim|bitco).(in|org).*|.+\/===/ig.test(window.location.href)
     if(!autofcb){
         refresh()
     }
