21b8a006bd866b70c2ed4a24975d2518e52c54c2
HARFHO <Harfho77@gmail.com>
2021-09-22 22:29:19 +0100

    Update AutoFCB(ReloadShortlinkPage).user.js

--- AutoFCB(ReloadShortlinkPage).user.js
+++ AutoFCB(ReloadShortlinkPage).user.js
@@ -2,22 +2,20 @@
     'use strict';
     function refresh(){
         let tryagain;
+        var time = 4
         tryagain=sessionStorage.getItem('tryagain')
         if(sessionStorage.getItem('tryagain')==null){sessionStorage.setItem('tryagain',1);tryagain=sessionStorage.getItem('tryagain')};
-        if(parseInt(tryagain)<=4){
-            GM_addElement(document.getElementsByTagName('head')[0], 'meta',{'http-equiv':"refresh",'content':"120;url="+window.location.href,});//content-in-seconds
+        if(parseInt(tryagain)<=time){
+            GM_addElement(document.getElementsByTagName('head')[0], 'meta',{'http-equiv':"refresh",'content':"120"});//content-in-seconds
+            document.title='r-' + document.title;
             sessionStorage.setItem('tryagain',parseInt(tryagain)+1)
         }
         else{
-            window.addEventListener('beforeunload', function (e) {
-                e.preventDefault();
-                sessionStorage.removeItem('tryagain');delete(sessionStorage.tryagain)
-            });
             sessionStorage.removeItem('tryagain');delete(sessionStorage.tryagain)
             //console.log('reset')
             GM_notification({
                 title:window.location.host,
-                text:window.location.href +' \nReloaded 4-times and will close after 10 seconds ---',
+                text:window.location.href +' \nReloaded'+ time +'-times and will close after 10 seconds ---',
                 timeout:10*1000,
                 ondone:()=>{delete(sessionStorage.tryagain);window.close()},
                 onclick:()=>{delete(sessionStorage.tryagain);window.open(window.location.href);window.close()}
