022b51dbaf22d09ed5f37595ed1e92b918878d68
HARFHO <Harfho77@gmail.com>
2021-10-14 01:23:10 +0100

    Update AutoFCB(UrlReplace).user.js

--- AutoFCB(UrlReplace).user.js
+++ AutoFCB(UrlReplace).user.js
@@ -25,7 +25,7 @@
         else if(/crazyblog.in\/\?postid=/ig.test(url)){window.location = h.search.replace(/.*=/,'')}
         else if(/11bit.co.in/.test(url)){window.location='http:/'+h.pathname.replace(/bitshort/,'bitshort.co')}
         else if(/trxking.xyz\/bonus/.test(url)){window.location='http:/'+h.pathname.replace(/\/bonus/,'shrinkpay.crazyblog.in/')}
-        else if(/.+clickscoin.com\/.+ccsl\/(.*)/ig.test(url)){path=h.pathname.replace(/\/ccsl\//,'');return 'ccsl.xyz/'+path }
+        else if(/.+clickscoin.com\/.+ccsl\/(.*)/ig.test(url)){return 'https://ccsl.xyz/'+($1) }
         //-----------------meta-----------------------------------------------------//
         else if(url.includes('themezon.net')){
             path = h.search.replace(/\?/,'')
