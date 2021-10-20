b7e4084fbeca642fb659546fc8f555c21faa06da
HARFHO <Harfho77@gmail.com>
2021-10-20 04:46:00 +0100

    Update AutoFCB(Close).user.js

--- AutoFCB(Close).user.js
+++ AutoFCB(Close).user.js
@@ -1,3 +1,5 @@
 (function () {
+    function OnPhone(){window.close();window.close();window.close();window.close()}
     "true"==sessionStorage.getItem("close")&&(window.close(),window.close(),window.close(),window.close(),window.close(),window.close());
+    OnPhone()
 })();
