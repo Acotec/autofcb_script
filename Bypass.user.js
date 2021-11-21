0e8e3267121a11351841b68c235460c34308bc44
HARFHO <Harfho77@gmail.com>
2021-11-21 06:13:10 +0100

    Update Bypass.user.js

--- Bypass.user.js
+++ Bypass.user.js
@@ -6,9 +6,11 @@
     var tryagain=sessionStorage.getItem('tryagain')
     if(/undefined|null/ig.test(sessionStorage.getItem('tryagain'))){sessionStorage.setItem('tryagain',1);tryagain=sessionStorage.getItem('tryagain')}
     let key =atob(GM_getResourceText("key").match(/\w*/gi).filter(e=>""!=e)[0])//get the api and decrypt it using btoa>atob
-    function bypass(l){
+    let retry = 4
+    function bypass(link){
         const api = 'https://api.yuumari.com/alpha-bypass/';
         const u =key
+        const l = link
         GM.xmlHttpRequest({
             method: 'POST',
             headers: {'Content-Type': 'application/x-www-form-urlencoded',},
@@ -17,10 +19,11 @@
             onload: r => {
                 let d =JSON.parse(r.responseText)
                 if(d.message==''){
+                    console.log('successfuly bypass')
                     sessionStorage.removeItem('tryagain')
                     window.location=d.result
                 }else{
-                    if(parseInt(tryagain)<=3){
+                    if(parseInt(tryagain)<=retry){
                         sessionStorage.setItem('tryagain',parseInt(tryagain)+1);
                         window.location.reload(true)
                     }
@@ -42,8 +45,8 @@
     }
     if(/\/===$/.test(window.location.href)){
         if(/megaurl.in\/bypass=/.test(window.location.href)){
-            const link=window.location.pathname.replace(/.*bypass=/,'').replace(/\/===/ig,'');
-            document.title =link
+            const link=window.location.pathname.replace(/.*bypass=|\/===/ig,''); //get the exact link to pass to bypasser
+            document.title =new URL(link).host;
             bypass(link)
         }else{
             const link = window.location.href.replace(/\/===/ig,'');
