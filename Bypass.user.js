93ed5e1b83a10e87033554325f7bfb283a121bbf
HARFHO <Harfho77@gmail.com>
2021-12-03 17:09:20 +0100

    update Bypass to use fetch.
     refer to  https://github.com/Acotec/autofcb_script/blob/0e8e3267121a11351841b68c235460c34308bc44/Bypass.user.js 
    it use  GM.xmlHttpRequest

--- Bypass.user.js
+++ Bypass.user.js
@@ -1,51 +1,47 @@
 (function() {
     if ( window.history.replaceState ) {
-    window.history.replaceState( null, null, window.location.href )
-    }//to prevent a resubmit on refresh and back button
+        window.history.replaceState( null, null, window.location.href )
+    }//to prevent resubmit on refresh and back button
     //---------------------------------------------------------//
-    var tryagain=sessionStorage.getItem('tryagain')
-    if(/undefined|null/ig.test(sessionStorage.getItem('tryagain'))){sessionStorage.setItem('tryagain',1);tryagain=sessionStorage.getItem('tryagain')}
-    let key =atob(GM_getResourceText("key").match(/\w*/gi).filter(e=>""!=e)[0])//get the api and decrypt it using btoa>atob
-    let retry = 4
+    var retry = 4
     function bypass(link){
-        const api = 'https://api.yuumari.com/alpha-bypass/';
-        const u =key
+        const key=atob(GM_getResourceText("key").match(/\w*/gi).filter(e=>""!=e)[0])
+        const baseUrl = 'https://api.yuumari.com/alpha-bypass/';
+        const u = key;
         const l = link
-        GM.xmlHttpRequest({
+        fetch(baseUrl, {
             method: 'POST',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded',},
-            url: api,
-            data: new URLSearchParams({u,l}).toString(),
-            onload: r => {
-                let d =JSON.parse(r.responseText)
-                if(d.message==''){
-                    console.log('successfuly bypass')
+            body: new URLSearchParams({u,l})
+        }).then(r => r.json())
+            .then((d)=>{
+            if(!d.message){
+                sessionStorage.removeItem('tryagain')
+                window.location=d.result
+
+            }else{
+                let tryagain;
+                tryagain=sessionStorage.getItem('tryagain')
+                if(sessionStorage.getItem('tryagain')==null){sessionStorage.setItem('tryagain',1);tryagain=sessionStorage.getItem('tryagain')}
+                if(parseInt(tryagain)<=retry){
+                    sessionStorage.setItem('tryagain',parseInt(tryagain)+1);
+                    window.location.reload(true)}
+                else{
                     sessionStorage.removeItem('tryagain')
-                    window.location=d.result
-                }else{
-                    if(parseInt(tryagain)<=retry){
-                        sessionStorage.setItem('tryagain',parseInt(tryagain)+1);
-                        window.location.reload(true)
-                    }
-                    else{
-                        sessionStorage.removeItem('tryagain')
-                        GM_notification({
-                            title:'!Bypass-- '+window.location.host,
-                            text:d.message+"--"+l,
-                            timeout:300*1000,
-                            ondone:()=>{window.close()},
-                            onclick:()=>{window.close()}
-                        });
-                        GM_setClipboard(l,{type:'text/plain'})
-                        window.close()
-                    }
+                    GM_notification({
+                        title:'!Bypass-- '+window.location.host,
+                        text:d.message+"--"+l,
+                        timeout:300*1000,
+                        ondone:()=>{window.close()},
+                    });
+                    GM_setClipboard(l,{type:'text/plain'})
+                    window.close()
                 }
             }
         });
     }
     if(/\/===$/.test(window.location.href)){
         if(/megaurl.in\/bypass=/.test(window.location.href)){
-            const link=window.location.pathname.replace(/.*bypass=|\/===/ig,''); //get the exact link to pass to bypasser
+            const link=window.location.pathname.replace(/.*bypass=/,'').replace(/\/===/ig,'');//get the exact link to pass to bypasser
             document.title =new URL(link).host;
             bypass(link)
         }else{
