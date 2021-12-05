8cec76e01f30eeb5b2a2eb09948c918c4e129d0e
HARFHO <Harfho77@gmail.com>
2021-12-05 07:33:02 +0100

    Update AutoFCB(DelayPage).user.js

--- AutoFCB(DelayPage).user.js
+++ AutoFCB(DelayPage).user.js
@@ -1,9 +1,9 @@
 (function() {
     'use strict';
-    var delayOn =GM_getResourceText("delaypage").replace(/'|"|\[|\]|\s/ig,'').split(',')
+    var delayOn =GM_getResourceText("delaypage").replace(/'|"|\[|\]|\s/ig,'').split(',').filter(e=>e);
     var host = window.location.host.toLowerCase().replace(/https:\/\/|www\.|\[^.*]|\/.*/ig, '')
     var delay = delayOn.some((link) => {return new RegExp(link,"ig").test(host)})
-    console.log(delay)
+    //console.log(delay)
     function sleep(e) {
         const startPoint = new Date().getTime()
         while (new Date().getTime() - startPoint <= e*1e3){}
