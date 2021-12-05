5b78761fe2bb5ea56cbbdb155fe40d35bc7a5112
HARFHO <Harfho77@gmail.com>
2021-12-05 09:13:09 +0100

    Update AutoFCB(DelayPage).user.js

--- AutoFCB(DelayPage).user.js
+++ AutoFCB(DelayPage).user.js
@@ -2,8 +2,10 @@
     'use strict';
     var delayOn =GM_getResourceText("delaypage").replace(/'|"|\[|\]|\s/ig,'').split(',').filter(e=>e);
     var host = window.location.host.toLowerCase().replace(/https:\/\/|www\.|\[^.*]|\/.*/ig, '')
-    var delay = delayOn.some((link) => {return new RegExp(link,"ig").test(host)})
-    //console.log(delay)
+    var url = window.location.href.toLowerCase().match(/https:\/\/|www\.|\/.*\/.+/ig).pop()
+    var delayhost = delayOn.some((link) => {return new RegExp(link,"ig").test(host)})
+    var delayurl = delayOn.some((link) => {return new RegExp(link,"ig").test(url)})
+    //console.log(delayOn,delayhost,delayurl)
     function sleep(e) {
         const startPoint = new Date().getTime()
         while (new Date().getTime() - startPoint <= e*1e3){}
@@ -13,8 +15,11 @@
         var inter=setTimeout(()=>{location.reload(false)},25*1000)
         null!=sessionStorage.getItem("reloaded")||sleep(17);
         sessionStorage.setItem('reloaded', 'yes');
-    } else if (delay && !/adbull/.test(host)) {
-        document.title ='D-'+host
+    } else if (delayhost&& !/adbull/.test(host)) {
+        document.title ='Dh-'+host
+        sleep(17);
+    } else if (delayurl) {
+        document.title ='Du-'+host+url
         sleep(17);
     }
 })();
