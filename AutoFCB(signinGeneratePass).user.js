2a313f62cab72326e708e9fdde8ed0fabed056ec
HARFHO <Harfho77@gmail.com>
2022-03-09 02:01:28 +0100

    Update AutoFCB(signinGeneratePass).user.js

--- AutoFCB(signinGeneratePass).user.js
+++ AutoFCB(signinGeneratePass).user.js
@@ -1,17 +1,3 @@
-// ==UserScript==
-// @name         AutoFCB(signinGeneratePass)
-// @namespace    Acotec
-// @version      0.1
-// @description  Genrate password from username
-// @author       Acotec
-// @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(signinGeneratePass).user.js
-// @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(signinGeneratePass).user.js
-// @include      *auto*/signin
-// @icon         https://www.google.com/s2/favicons?domain=autofaucet.org
-// @grant        none
-// @run-at       document-end
-// ==/UserScript==
-
 (function() {
     let form = Array.from(document.querySelectorAll(".form-control"))
     let ps="",
