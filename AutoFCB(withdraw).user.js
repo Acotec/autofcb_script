10921d163a2e7b87ca1794c1c7eb59f38b1d486d
HARFHO <Harfho77@gmail.com>
2022-03-25 04:04:55 +0100

    Update AutoFCB(withdraw).user.js

--- AutoFCB(withdraw).user.js
+++ AutoFCB(withdraw).user.js
@@ -1,94 +1,121 @@
 (function() {
     'use strict';
-    var coinS=GM_getValue("withdraw_coin",false);
+    Array.prototype.sample = function() {return this[Math.floor(Math.random() * this.length)];}
+    var coinS = GM_getValue("withdraw_coin", null);
+    var fpcoins = ["LTC", "DASH", "BCH", "DGB", "TRX", "USDT", "ZEC", "BNB", "SOL", "DOGE"].sample() //"BTC","ETH"
     var loop = 0
-    function selectFromDropDown(id,value){
+    if (coinS) {GM_setValue("withdraw_coin", coinS)} else {GM_setValue("withdraw_coin", fpcoins)}
+    function selectFromDropDown(id, value) {
         //alert(value)
-        if(String(value)!=='undefined'){
+        if (String(value) !== 'undefined') {
             let element = document.querySelector(id);
-            Array.from(element.options).filter(a=>{if(RegExp(value,'ig').test(a.innerText.replace(/\s/ig,''))){a.selected = true;element.dispatchEvent(new Event('change'));}});
-        }else{
+            Array.from(element.options).filter(a => {
+                if (RegExp(value, 'ig').test(a.innerText.replace(/\s/ig, ''))) {
+                    a.selected = true;
+                    element.dispatchEvent(new Event('change'));
+                }
+            });
+        } else {
             console.log('No currency claim yet ')
         }
     }
     waitForKeyElements('#estimatedc', (element) => {
-        coinS=element.innerText.replace(/[\W\d]+/ig,"")
-        GM_setValue("withdraw_coin",coinS)
-    },false);
+        coinS = element.innerText.replace(/[\W\d]+/ig, "")
+        GM_setValue("withdraw_coin", coinS)
+    }, false);
     waitForKeyElements('.toast-message', (element) => {
-        if(/payment has been sent/gi.test(element.innerHTML) && /dashboard\/withdraw\/\w+/gi.test(window.location.href)){
+        if (/payment has been sent/gi.test(element.innerHTML) && /dashboard\/withdraw\/\w+/gi.test(window.location.href)) {
             // let url='https://'+ window.location.host +'/dashboard/shortlinks'//'https://'+ window.location.host +'/dashboard/claim/manual'
             // window.location=url
             window.location.reload()
-        }else if(/credited to your balance/gi.test(element.innerHTML) && /claim\/manual/gi.test(window.location.href)){
-            let url='https://'+ window.location.host +'/dashboard/withdraw/'+ GM_getValue("withdraw_coin")
-            window.location=url
-        }else if(/You entered an incorrect answer for the captcha, please try again|wrong.?answer/.test(element.innerHTML)){
+        } else if (/credited to your balance/gi.test(element.innerHTML) && /claim\/manual/gi.test(window.location.href)) {
+            GM_setValue("withdraw_coin", null)
+            let url = 'https://' + window.location.host + '/dashboard/withdraw/' + GM_getValue("withdraw_coin")
+            window.location = url
+        } else if (/we can not process your payment right now/ig.test(element.innerHTML)) {
+            GM_setValue("withdraw_coin", null)
+            window.location.reload()
+        } else if (/You entered an incorrect answer for the captcha, please try again|wrong.?answer/.test(element.innerHTML)) {
             window.location.reload(false)
-        }else if(/You have not entered your FaucetPay E-mail address yet/ig.test(element.innerHTML)){
-            window.location.href = 'https://'+ window.location.host +'/dashboard/settings'
+        } else if (/You have not entered your FaucetPay E-mail address yet/ig.test(element.innerHTML)) {
+            window.location.href = 'https://' + window.location.host + '/dashboard/settings'
         }
-    },false);
+    }, false);
 
-    if(/claim\/manual/gi.test(window.location.href)){
-        let def_coin= "USDT"
-        selectFromDropDown('#currency-select',def_coin)
-        if(coinS==false){GM_setValue("withdraw_coin",def_coin)}
-        let inter=setInterval(function () {
-            loop+=1
-            let token = document.querySelector("#token-amount").value==0
-            if(token&&!(loop>=50)){
+    if (/claim\/manual/gi.test(window.location.href)) {
+        //let def_coin= "USDT"
+        //selectFromDropDown('#currency-select',def_coin)
+        //if(coinS==null){GM_setValue("withdraw_coin",def_coin)}
+        let inter = setInterval(function() {
+            loop += 1
+            let token = document.querySelector("#token-amount").value == 0
+            if (token && !(loop >= 50)) {
                 document.querySelector("#maxmcla-addon").click()
-            }
-            else{
-                clearInterval(inter);clearInterval(inter)
-                if(!token){
-                    selectFromDropDown('#currency-select',GM_getValue("withdraw_coin"))
-                    selectFromDropDown('#captcha-select','solvemedia')}
-            }
-        },100)
-        } else if(/dashboard\/withdraw\/\w+/gi.test(window.location.href)){
-            let inter=setInterval(function () {
-                loop+=1
-                let amount = document.querySelector("#amount").value;
-                let coinname = document.querySelector("p.amount").innerText.replace(/[\W\d]/ig,"")
-                GM_setValue( "available_balance",amount);
-                GM_setValue( "coin",coinname);
-                amount=amount==0
-                if(amount&&!(loop>=50)){
-                    document.querySelector("#maxwith-addon").click()
+            } else {
+                clearInterval(inter);
+                clearInterval(inter)
+                if (!token) {
+                    selectFromDropDown('#currency-select', GM_getValue("withdraw_coin"))
+                    selectFromDropDown('#captcha-select', 'solvemedia')
                 }
-                else{
-                    clearInterval(inter);clearInterval(inter)
-                    if(!amount){
-                        selectFromDropDown('#processor','faucetpay')
-                        selectFromDropDown('#captcha-select','solvemedia')}
+            }
+        }, 100)
+        } else if (/dashboard\/withdraw\/\w+/gi.test(window.location.href)) {
+            let inter = setInterval(function() {
+                loop += 1
+                let amount = document.querySelector("#amount").value;
+                let coinname = document.querySelector("p.amount").innerText.replace(/[\W\d]/ig, "")
+                GM_setValue("available_balance", amount);
+                GM_setValue("coin", coinname);
+                amount = amount == 0
+                if (amount && !(loop >= 50)) {
+                    document.querySelector("#maxwith-addon").click()
+                } else {
+                    clearInterval(inter);
+                    clearInterval(inter)
+                    if (!amount) {
+                        selectFromDropDown('#processor', 'faucetpay')
+                        selectFromDropDown('#captcha-select', 'solvemedia')
+                    }
 
                 }
-            },100);
-            let url='https://'+ window.location.host +'/dashboard/shortlinks';
+            }, 100);
+            let url = 'https://' + window.location.host + '/dashboard/shortlinks';
             waitForKeyElements(".error-desc", (element) => {
-                if(/.*entered your E-mail address that you registered to FaucetPay.*/ig.test(element.textContent)){
-                    window.location.href='https://'+window.location.host+"/dashboard/settings"
+                if (/.*entered your E-mail address that you registered to FaucetPay.*/ig.test(element.textContent)) {
+                    window.location.href = 'https://' + window.location.host + "/dashboard/settings"
                 }
             });
-            if(/0.0000000+/.test(document.querySelector("#availableBalance").innerText)){setTimeout(()=>{window.location.href=url},1000)}
-        } else if(/dashboard\/settings/ig.test(window.location.href)){
+            if (/0.0000000+/.test(document.querySelector("#availableBalance").innerText)) {
+                setTimeout(() => {
+                    window.location.href = url
+                }, 1000)
+            }
+        } else if (/dashboard\/settings/ig.test(window.location.href)) {
             waitForKeyElements('.input-group', (element) => {
                 let Email = element.getElementsByTagName('input')[0].value
-                GM_setValue('Email',Email)
-                window.location = 'https://'+ window.location.host+"/dashboard/withdraw#settings"
+                GM_setValue('Email', Email)
+                window.location = 'https://' + window.location.host + "/dashboard/withdraw#settings"
             });
-        } else if(/dashboard\/withdraw\#settings/ig.test(window.location.href)){
-            var saveButton=[]
+        } else if (/dashboard\/withdraw\#settings/ig.test(window.location.href)) {
+            var saveButton = []
             waitForKeyElements('#form-faucetpaymail', (element) => {
                 let faucetpayemail = element.querySelector("#faucetpay-email")
-                faucetpayemail.value=GM_getValue('Email')
+                faucetpayemail.value = GM_getValue('Email')
                 var saveEmail = Array.from(document.querySelectorAll("button"));
-                saveEmail.filter((b)=>{if(/save/ig.test(b.innerText)){saveButton.push(b)}})
-                saveButton=saveButton.pop()
+                saveEmail.filter((b) => {
+                    if (/save/ig.test(b.innerText)) {
+                        saveButton.push(b)
+                    }
+                })
+                saveButton = saveButton.pop()
             });
-            setTimeout(()=>{saveButton.click();document.dispatchEvent(new Event('change'))},3000)
-            setTimeout(()=>{window.location ='https://'+ window.location.host +'/dashboard/withdraw/'+ GM_getValue("withdraw_coin")},4000)
+            setTimeout(() => {
+                saveButton.click();
+                document.dispatchEvent(new Event('change'))
+            }, 3000)
+            setTimeout(() => {
+                window.location = 'https://' + window.location.host + '/dashboard/withdraw/' + GM_getValue("withdraw_coin")
+            }, 4000)
         }
 })();
