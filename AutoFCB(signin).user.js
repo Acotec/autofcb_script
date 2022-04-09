433bf69cd0d93eb93c5c4ced1b55625b70f014b3
HARFHO <Harfho77@gmail.com>
2022-04-09 13:08:14 +0100

    Update AutoFCB(signin).user.js

--- AutoFCB(signin).user.js
+++ AutoFCB(signin).user.js
@@ -1,8 +1,9 @@
 (function() {
     'use strict';
     Array.prototype.sample = function() {return this[Math.floor(Math.random() * this.length)];}
-    var captchatoselect = ['solvemedia'].sample()//"recaptcha",
+    var captchatoselect = ["recaptcha",'solvemedia'].sample()
     var loop=0
+    var repeat = 3
     if(/autofaucet.org\/auth\/signin/ig.test(window.location.href)){
         setInterval((e=document.querySelector("#submit"))=>{
             console.log(e.innerText);if(/Signing.*in/ig.test(e.innerText)){window.location.reload(false)}},2000)
@@ -11,11 +12,11 @@
             var captcha_select = document.querySelector('#captcha-select')
             var captcha= Array.from(captcha_select.children)
             captcha.forEach((e)=>{if(new RegExp(captchatoselect,'ig').test(e.innerText)){e.click();e.selected = true;e.dispatchEvent(new Event('click'))}})
-            if(loop>20&&new RegExp(captchatoselect,'ig').test(document.querySelector("#dropdownMenuButton").innerText)){
+            if(loop>=repeat&&new RegExp(captchatoselect,'ig').test(document.querySelector("#dropdownMenuButton").innerText)){
                 clearInterval(check)
             }
 
-        },100)
+        },500)
         }
     else{
         function selectFromDropDown(id,value){
@@ -24,10 +25,14 @@
                 let element = document.querySelector(id);
                 Array.from(element.options).filter(a=>{if(new RegExp(value,'ig').test(a.innerText.replace(/\s/ig,''))){a.selected = true;element.dispatchEvent(new Event('change'));}});
             }}
-        //setInterval(()=>{selectFromDropDown('#captcha-select',captchatoselect)},1000)
-        selectFromDropDown('#captcha-select',captchatoselect)
-        //let check =setInterval(()=>{loop++;selectFromDropDown('#captcha-select',captchatoselect);loop>50||clearInterval(check);;console.log('reselect solve_media')},3000)
-        setTimeout(()=>{selectFromDropDown('#captcha-select',captchatoselect);console.log('reselect solve_media')},3000)
+        //selectFromDropDown('#captcha-select',captchatoselect)
+        let check =setInterval(()=>{
+            loop++;
+            selectFromDropDown('#captcha-select',captchatoselect);
+            console.log('reselect',captchatoselect)
+            loop<=repeat||clearInterval(check);
+        },500)
+        //setTimeout(()=>{selectFromDropDown('#captcha-select',captchatoselect);console.log('reselect',captchatoselect)},3000)
         setInterval((e=document.querySelector(".toast"))=>{try{if(/error/ig.test(e.innerText)){window.location.reload(false)}}catch(e){null}},2000)
     }
 })();
